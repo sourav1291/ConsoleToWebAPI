53a6bdbdd5eb8d543488b8cd717306f145ae6eb8
Sourav Pandey <souravkumar.pandey@siemens.com>
2022-01-05 00:14:15 +0530

    Added controller and startup class.

---
+++ ConsoleToWebAPI/Controllers/TestController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConsoleToWebAPI.Controllers
+{
+    [Route("test/{action}", Name = "test", Order = 1)]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        public string Get()
+        {
+            return "Returning from TestController Get Method";
+        }
+        public string Get1()
+        {
+            return "Returning from TestController Get1 Method";
+        }
+    }
+}
--- ConsoleToWebAPI/Startup.cs
+++ ConsoleToWebAPI/Startup.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace ConsoleToWebAPI
 {
@@ -13,16 +14,22 @@ namespace ConsoleToWebAPI
         }
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
             app.UseRouting();
             app.UseEndpoints(endPoints =>
             {
-                endPoints.MapGet("/", async context => {
-                    await context.Response.WriteAsync("Hello From ASP.NET Core Web API");
-                });
+                //endPoints.MapGet("/", async context => {
+                //    await context.Response.WriteAsync("Hello From ASP.NET Core Web API");
+                //});
 
-                endPoints.MapGet("/Resource1", async context => {
-                    await context.Response.WriteAsync("Hello From ASP.NET Core Web API Resource1");
-                });
+                //endPoints.MapGet("/Resource1", async context => {
+                //    await context.Response.WriteAsync("Hello From ASP.NET Core Web API Resource1");
+                //});
+                endPoints.MapControllers();
             });
         }
     }
