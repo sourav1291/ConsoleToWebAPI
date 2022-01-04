c387e4720b31e9c8e2b8eb0267f3c49eb4cf5bb8
Sourav Pandey <souravkumar.pandey@siemens.com>
2022-01-04 16:08:36 +0530

    Add project files.

---
+++ ConsoleToWebAPI/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Hosting;
+
+namespace ConsoleToWebAPI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+        public static IHostBuilder CreateHostBuilder(string[] args) => Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseStartup<Startup>();
+            });
+    }
+}
---
+++ ConsoleToWebAPI/Startup.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ConsoleToWebAPI
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+        }
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseRouting();
+            app.UseEndpoints(endPoints =>
+            {
+                endPoints.MapGet("/", async context => {
+                    await context.Response.WriteAsync("Hello From ASP.NET Core Web API");
+                });
+
+                endPoints.MapGet("/Resource1", async context => {
+                    await context.Response.WriteAsync("Hello From ASP.NET Core Web API Resource1");
+                });
+            });
+        }
+    }
+}
